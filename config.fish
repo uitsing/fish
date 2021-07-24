6ee1c0cd550b9f9208c40ab7c538d13e0f4cb2d4
uitsing <tsing@hey.com>
2021-07-24 15:07:21 +0000

    add homebrew path and environments

--- config.fish
+++ config.fish
@@ -1,5 +1,17 @@
 if status is-interactive
-    eval (/opt/homebrew/bin/brew shellenv)
+    # homebrew path and environments
+    # eval (/opt/homebrew/bin/brew shellenv)
+    set -gx HOMEBREW_PREFIX "/opt/homebrew";
+    set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+    set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+    set -q PATH; or set PATH ''
+    set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+    set -q MANPATH; or set MANPATH ''
+    set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+    set -q INFOPATH; or set INFOPATH ''
+    set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+
     set -U fish_greeting
     alias vi="nvim"
     alias rm="trash"
